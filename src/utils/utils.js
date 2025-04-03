628e4c34a583ebc343b62f6276f11152fdb44b27
Lewis Bull <lewisbull2303@gmail.com>
2025-04-03 16:50:27 +0100

    Created a function that will remove the refresh token value from the local storage if the user logs out or the token expires

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -72,3 +72,7 @@ export const setTokenTimestamp = (data) => {
 export const shouldRefreshToken = () => {
     return !!localStorage.getItem("refreshTokenTimestamp");
     };
+
+export const removeTokenTimestamp = () => {
+    localStorage.removeItem("refreshTokenTimestamp");
+    };
