b9d295ef2880b29f6db87080908a3349ec5ae38f
Lewis Bull <lewisbull2303@gmail.com>
2025-04-04 12:21:48 +0100

    Added media quiery styles for my profile page

--- src/pages/profiles/profile.js
+++ src/pages/profiles/profile.js
@@ -5,3 +5,51 @@ import Avatar from "../../components/avatar";
 import { Button } from "react-bootstrap";
 import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
+const Profile = (props) => {
+    const { profile, imageSize = 40 } = props;
+    const { id, following_id, image, owner } = profile;
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+  
+    const { handleFollow, handleUnfollow } = useSetProfileData();
+  
+    return (
+      <div
+        className={`my-3 d-flex align-items-center`}
+      >
+        <div>
+          <Link to={`/profiles/${id}`}>
+            <Avatar src={image} height={imageSize} />
+          </Link>
+        </div>
+        <div>
+          <Link to={`/profiles/${id}`}>
+            <strong>{owner}</strong>
+          </Link>
+        </div>
+  
+        <div className={` ml-auto`}>
+          {/* Displays follow/unfollow buttons and do not allow users to follow themselves */}
+          {currentUser &&
+            !is_owner &&
+            (following_id ? (
+              <Button
+                className={``}
+                onClick={() => handleUnfollow(profile)}
+              >
+                unfollow
+              </Button>
+            ) : (
+              <Button
+                className={``}
+                onClick={() => handleFollow(profile)}
+              >
+                follow
+              </Button>
+            ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Profile;
