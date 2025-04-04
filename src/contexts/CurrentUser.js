7f494020a136b6f1fcd55ac15a38bdbfa419f352
Lewis Bull <lewisbull2303@gmail.com>
2025-04-04 08:58:36 +0100

    Added a function to handle the users acces tokens and send then to the login page if the token cannot be refreshed

--- src/contexts/CurrentUser.js
+++ src/contexts/CurrentUser.js
@@ -30,3 +30,62 @@ export const CurrentUserProvider = ({ children }) => {
       useEffect(() => {
         handleMount();
       }, []);
+
+    /* 
+    Handles access tokens
+    Redirects user to log-in page if refreshing of token fails
+    */
+    useMemo(() => {
+        axiosReq.interceptors.request.use(
+        async (config) => {
+            if (shouldRefreshToken()) {
+            try {
+                await axios.post("/dj-rest-auth/token/refresh/");
+            } catch (err) {
+                setCurrentUser((prevCurrentUser) => {
+                if (prevCurrentUser) {
+                    history.push("/login");
+                }
+                return null;
+                });
+                removeTokenTimestamp();
+                return config;
+            }
+            }
+            return config;
+        },
+        (err) => {
+            return Promise.reject(err);
+        }
+        );
+
+        axiosRes.interceptors.response.use(
+            (response) => response,
+            async (err) => {
+              if (err.response?.status === 401) {
+                try {
+                  await axios.post("/dj-rest-auth/token/refresh/");
+                } catch (err) {
+                  setCurrentUser((prevCurrentUser) => {
+                    if (prevCurrentUser) {
+                      history.push("/login");
+                    }
+                    return null;
+                  });
+                  removeTokenTimestamp();
+                }
+                // if no error refreshing the token
+                return axios(err.config);
+              }
+              return Promise.reject(err);
+            }
+          );
+        }, [history]);
+        return (
+            <CurrentUserContext.Provider value={currentUser}>
+              <SetCurrentUserContext.Provider value={setCurrentUser}>
+                {children}
+              </SetCurrentUserContext.Provider>
+            </CurrentUserContext.Provider>
+          );
+        };
