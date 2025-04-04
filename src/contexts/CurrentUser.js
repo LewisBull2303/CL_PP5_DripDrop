8c35a237be3513d6dbe7710b38b7bce720491c6b
Lewis Bull <lewisbull2303@gmail.com>
2025-04-04 08:47:39 +0100

    Created a function that takes the current users request and mounts it and then completes the request

--- src/contexts/CurrentUser.js
+++ src/contexts/CurrentUser.js
@@ -9,3 +9,24 @@ export const SetCurrentUserContext = createContext();
 
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+
+
+export const CurrentUserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const history = useHistory();
+  /*
+    Handles the request of current users data from the API
+    completes the request when the component is mounted
+  */
+    const handleMount = async () => {
+        try {
+          const { data } = await axiosRes.get("dj-rest-auth/user/");
+          setCurrentUser(data);
+        } catch (err) {
+          // console.log(err);
+        }
+      };
+    
+      useEffect(() => {
+        handleMount();
+      }, []);
