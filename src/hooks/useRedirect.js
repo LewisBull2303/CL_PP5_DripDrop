e9dbeaa18dca23075deb4a3f1148d1126d06e347
Lewis Bull <lewisbull2303@gmail.com>
2025-04-11 09:39:06 +0100

    added my handlemount function to the useRedirect with the refresh tokens

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -4,4 +4,20 @@ import { useHistory } from "react-router";
 
 export const useRedirect = (userAuthStatus) => {
     const history = useHistory();
-}
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+            if (userAuthStatus === "loggedIn") {
+              history.push("/");
+            }
+          } catch (err) {
+            if (userAuthStatus === "loggedOut") {
+              history.push("/");
+            }
+          }
+        };
+    
+        handleMount();
+      }, [history, userAuthStatus]);
+    };
