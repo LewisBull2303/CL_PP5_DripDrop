9a13d79fa8bc03fef54d90dd19148acaa77590f9
Lewis Bull <lewisbull2303@gmail.com>
2025-04-11 14:41:27 +0100

    Imported the password criteria to the signup form and created the signup form function

--- src/pages/auth/SignupForm.js
+++ src/pages/auth/SignupForm.js
@@ -4,4 +4,17 @@ import styles from "../../styles/LogInSignUpForm.module.css";
 import appStyles from "../../App.module.css";
 import { Alert, Form, Button, Col, Row, Container } from "react-bootstrap";
 import axios from "axios";
+import PasswordCriteria from "../../components/PasswordCriteria";
 import { useRedirect } from "../../hooks/useRedirect";
+
+const SignUpForm = () => {
+  useRedirect("loggedIn");
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+}
