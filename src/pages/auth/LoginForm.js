2d7874faaea782ed42652721702bfd0992438236
Lewis Bull <lewisbull2303@gmail.com>
2025-04-11 09:33:49 +0100

    Added my loginform with the imports in there

---
+++ src/pages/auth/LoginForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { setTokenTimestamp } from "../../utils/utils";
+
+function LogInForm() {
+    const setCurrentUser = useSetCurrentUser();
+    useRedirect("loggedIn");
+}
