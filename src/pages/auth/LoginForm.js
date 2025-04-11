56735d3dcd463314cf4af1a658709a2095ba98da
Lewis Bull <lewisbull2303@gmail.com>
2025-04-11 10:56:06 +0100

    Added my styles into my loginform and added the loginform functions

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
+import appStyles from "../../main.styles.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
 import { setTokenTimestamp } from "../../utils/utils";
 
+function LogInForm() {
+    const setCurrentUser = useSetCurrentUser();
+    useRedirect("loggedIn");
+  
+    const [logInData, setLogInData] = useState({
+      username: "",
+      password: "",
+    });
+}
