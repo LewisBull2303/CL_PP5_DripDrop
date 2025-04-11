ba6271d1194e4434c210ba3041ade9a2bab20496
Lewis Bull <lewisbull2303@gmail.com>
2025-04-11 10:51:33 +0100

    Added my main styles css file that contains all of the base styles for the page

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -2,5 +2,6 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 import { setTokenTimestamp } from "../../utils/utils";
 
