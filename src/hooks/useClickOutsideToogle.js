306284749618efd61193c152fecd8ca3e939ecf1
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 12:03:38 +0100

    Added the click away function

--- src/hooks/useClickOutsideToogle.js
+++ src/hooks/useClickOutsideToogle.js
@@ -1,1 +1,29 @@
 import { useEffect, useRef, useState } from "react";
+
+const useClickOutsideToggle = () => {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null); // a variable that will hold a reference to the hamburger icon
+
+  /*
+    Handles user click away from the hamburger menu
+    Class 'exp' was applied to user avatar & navbar dropdown menu,
+    if clicked on either of them the navbar won't collapse
+  */
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (e.target?.classList.contains("exp")) return;
+      // check if the user has clicked away from the referenced button (hamburger icon)
+      if (ref.current && !ref.current.contains(e.target)) {
+        setExpanded(false);
+      }
+    };
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+
+  return { expanded, setExpanded, ref };
+};
+
+export default useClickOutsideToggle;
