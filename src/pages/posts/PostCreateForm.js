770f695541e89432a891a2acbfd023775a8df265
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 17:04:14 +0100

    Added the post create function and the handle change fucntion

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -14,3 +14,27 @@ import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import { useHistory } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+
+function PostCreateForm() {
+    const [errors, setErrors] = useState({});
+  
+    const [postData, setPostData] = useState({
+      title: "",
+      category: "",
+      description: "",
+      image: "",
+    });
+  
+    const { title, category, description, image } = postData;
+    const imageInput = useRef(null);
+    const history = useHistory();
+  
+    /* 
+      Handles changes to the create form input fields
+    */
+    const handleChange = (e) => {
+      setPostData({
+        ...postData,
+        [e.target.name]: e.target.value,
+      });
+    };
