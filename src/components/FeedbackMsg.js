e9323efa9034ee1b3fd904b9213937c5a9052d41
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 10:55:30 +0100

    Added the feedback message component and added the imports

---
+++ src/components/FeedbackMsg.js
@@ -0,0 +1,5 @@
+// https://blog.logrocket.com/create-custom-react-alert-message/
+import styles from "../styles/FeedbackMsg.module.css";
+import css from "classnames";
+import React from "react";
+import { useState } from "react";
