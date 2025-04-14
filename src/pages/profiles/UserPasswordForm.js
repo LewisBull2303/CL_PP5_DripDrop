ffda20657dff5e35c72a057343cc17d635065708
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 13:35:36 +0100

    Added the userpasswordform page and added in my imports

---
+++ src/pages/profiles/UserPasswordForm.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useHistory, useParams } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import appStyles from "../../App.module.css";
+import PasswordCriteria from "../../components/PasswordCriteria";
+import FeedbackMsg from "../../components/FeedbackMsg";
