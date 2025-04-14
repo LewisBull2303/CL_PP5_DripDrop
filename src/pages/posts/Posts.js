c9f267165391651a26231f8e0273d63337bc2426
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 14:28:35 +0100

    Added the handle delete function for the posts page

--- src/pages/posts/Posts.js
+++ src/pages/posts/Posts.js
@@ -40,4 +40,21 @@ const Post = (props) => {
     const handleEdit = () => {
       history.push(`/posts/${id}/edit`);
     };
+
+    /*
+    Handles deleting of the post
+    Shows the confirmation message to the user
+    Redirects the user to the main page after a short delay
+  */
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}/`);
+      setShowAlert(true);
+      setTimeout(function () {
+        history.push("/");
+      }, 1500);
+    } catch (err) {
+      // console.log(err);
+    }
+  };
   
