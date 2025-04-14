9d518b98d3a3bba54a1af5418a45a60dc69fc747
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 12:15:42 +0100

    Added a 404 page component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+import NotFound from "../assets/404-page.jpg";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import appStyles from "../App.module.css";
+import { Link } from "react-router-dom";
