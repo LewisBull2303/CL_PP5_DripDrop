aeb9362842c56fad23d4ba460b1315acf5b7ad6f
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 12:20:29 +0100

    Added the popular profiles page and added the imports

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/SmallMenuContainer.module.css";
+import Asset from "../../components/Asset";
+import Profile from "./Profile";
+import { useProfileData } from "../../contexts/ProfileDataContext";
