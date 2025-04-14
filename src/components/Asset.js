053bd818a5a5006fbc95b6cf4053c2a9b07931d4
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 09:52:49 +0100

    Added the asset function which will displate and item depending on what is passed through it

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -2,3 +2,18 @@ import React from "react";
 import styles from "../styles/Asset.module.css";
 import loading from "../assets/loading.gif";
 
+/*
+  Multipurpose component for different items
+  Display depends on the items passed
+*/
+const Asset = ({ spinner, src, message }) => {
+  return (
+    <div className={`${styles.Asset} p-4`}>
+      {spinner && <img src={loading} className={styles.spinner} alt="Loading..." />}
+      {src && <img src={src} alt={message} />}
+      {message && <p className="mt-3">{message}</p>}
+    </div>
+  );
+};
+
+export default Asset;
