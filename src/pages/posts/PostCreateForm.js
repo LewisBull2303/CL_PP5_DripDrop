ebfa5192fa69ee7f3bb9f180da6d252be2ccf224
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 16:55:46 +0100

    added the post create form file and added the imports

---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,16 @@
+import React, { useRef, useState } from "react";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  Image,
+  Row,
+} from "react-bootstrap";
+import Upload from "../../assets/upload-image.png";
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
