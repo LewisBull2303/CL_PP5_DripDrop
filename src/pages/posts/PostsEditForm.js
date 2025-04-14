f15b4669df83c370bb3206d50752ceec6751bd3a
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 15:10:58 +0100

    Added the handle mount functions which handles the api and gets the data the user wants to edit

--- src/pages/posts/PostsEditForm.js
+++ src/pages/posts/PostsEditForm.js
@@ -28,3 +28,26 @@ function PostEditForm() {
     const imageInput = useRef(null);
     const history = useHistory();
     const { id } = useParams(); // get a parameter out of the URL
+
+    /*
+    Handles API request using the post id parameter
+    Gets the data about the posts user wants to edit
+    Prevents editing other users' posts
+    and redirects to main page if attempted
+  */
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/${id}/`);
+        const { title, category, description, image, is_owner } = data;
+
+        is_owner
+          ? setPostData({ title, category, description, image })
+          : history.push("/");
+      } catch (err) {
+        //console.log(err)
+      }
+    };
+
+    handleMount();
+  }, [history, id]);
