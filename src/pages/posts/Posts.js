c78cc4fd95b99b23463eef35b7144b323b67ec46
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 14:16:01 +0100

    Added the posts.js file and imported all needed libraries

---
+++ src/pages/posts/Posts.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styles from "../../styles/Post.module.css";
+import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Badge, Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+import Like from "../../assets/like.png";
+import Unlike from "../../assets/unlike.png";
+import { DropdownMenu } from "../../components/DropdownMenu";
+import FeedbackMsg from "../../components/FeedbackMsg";
