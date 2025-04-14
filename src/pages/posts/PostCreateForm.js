47e6906d3afb0359ebafd7427941cb712cbced4f
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 17:12:36 +0100

    Added the options for the categories and added the description text input

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -116,3 +116,63 @@ function PostCreateForm() {
               onChange={handleChange}
               aria-label="category"
             >
+                <option value="Classic Casual">"Classic Casual"</option>
+                <option value="Sporty (Athleisure)">"Sporty (Athleisure)"</option>
+                <option value="Streetwear">"Streetwear"</option>
+                <option value="Bohemian (Boho)">"Bohemian (Boho)"</option>
+                <option value="Preppy">"Preppy"</option>
+                <option value="Hipster">"Hipster"</option>
+                <option value="Grunge">"Grunge"</option>
+                <option value="Minimalist">"Minimalist"</option>
+                <option value="Y2K Fashion">"Y2K Fashion"</option>
+                <option value="Soft Girl">"Soft Girl"</option>
+                <option value="VSCO Girl">"VSCO Girl"</option>
+                <option value="Cottagecore">"Cottagecore"</option>
+                <option value="E-Girl">"E-Girl"</option>
+                <option value="Skater">"Skater"</option>
+                <option value="Dark Academia">"Dark Academia"</option>
+                <option value="Light Academia">"Light Academia"</option>
+                <option value="Indie">"Indie"</option>
+                <option value="Business Casual">"Business Casual"</option>
+                <option value="Business Professional">"Business Professional"</option>
+                <option value="Smart Casual">"Smart Casual"</option>
+                <option value="Power Dressing">"Power Dressing"</option>
+                <option value="Punk">"Punk"</option>
+                <option value="Gothic">"Gothic"</option>
+                <option value="Cyberpunk">"Cyberpunk"</option>
+                <option value="Rockstar Chic">"Rockstar Chic"</option>
+                <option value="Steampunk">"Steampunk"</option>
+                <option value="Emo">"Emo"</option>
+                <option value="Gyaru">"Gyaru"</option>
+                <option value="Vintage">"Vintage"</option>
+                <option value="Retro">"Retro"</option>
+                <option value="Kawaii">"Kawaii"</option>
+                <option value="Harajuku">"Harajuku"</option>
+                <option value="Lolita">"Lolita"</option>
+                <option value="K-Pop Fashion">"K-Pop Fashion"</option>
+                <option value="Hippie">"Hippie"</option>
+                <option value="Winter Fashion">"Winter Fashion"</option>
+                <option value="Summer Fashion">"Summer Fashion"</option>
+                <option value="Resort Wear">"Resort Wear"</option>
+                <option value="Workwear (Utility Fashion)">"Workwear (Utility Fashion)"</option>
+                <option value="Avant-Garde">"Avant-Garde"</option>
+                <option value="Haute Couture">"Haute Couture"</option>
+                <option value="Luxury Chic">"Luxury Chic"</option>
+                <option value="Sci-Fi/Fantasy Fashion">"Sci-Fi/Fantasy Fashion"</option>
+                <option value="Rave/Festival Fashion">"Rave/Festival Fashion"</option>
+                <option value="Normcore">"Normcore"</option>
+            </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="description"
+          className={appStyles.Input}
+          value={description}
+          onChange={handleChange}
+          aria-label="post description"
+        />
+      </Form.Group>
