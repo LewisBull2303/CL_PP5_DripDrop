685612febcbacb84d47dd43c5dab33ae67e348c5
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 12:21:29 +0100

    Added the page structure for the popular profiles page which displays the 5 most followed profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,3 +5,29 @@ import styles from "../../styles/SmallMenuContainer.module.css";
 import Asset from "../../components/Asset";
 import Profile from "./Profile";
 import { useProfileData } from "../../contexts/ProfileDataContext";
+
+/*
+  Displays first five most followed profiles in the app
+*/
+const PopularProfiles = () => {
+  const { popularProfiles } = useProfileData();
+
+  return (
+    <Container className={`${appStyles.Content} font-weight-bold pt-1 pb-0`}>
+      {popularProfiles.results.length ? (
+        <>
+          <p>Top Users</p>
+          <div className={styles.UsersDisplay}>
+            {popularProfiles.results.slice(0, 5).map((profile) => (
+              <Profile key={profile.id} profile={profile} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
