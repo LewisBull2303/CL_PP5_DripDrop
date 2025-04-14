ee8cb0d6ae9135a13143b21cb9ef2a4ff79a259e
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 09:54:08 +0100

    Added my dropdown dots component which has the fontawesome ellipsis

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -2,3 +2,14 @@ import React from "react";
 import { Dropdown, Tooltip, OverlayTrigger } from "react-bootstrap";
 import styles from "../styles/DropdownMenu.module.css";
 import { useHistory } from "react-router";
+
+const DropdownDots = React.forwardRef(({ onClick }, ref) => (
+    <i
+      className="fas fa-ellipsis"
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    />
+  ));
