579ef7bf82b0f76548c9e2949a9d3fb42920e198
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 17:12:59 +0100

    Finalised the page structure of the post create form page

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -176,3 +176,83 @@ function PostCreateForm() {
           aria-label="post description"
         />
       </Form.Group>
+
+      <Button
+        className={`my-3 ${appStyles.button}`}
+        onMouseDown={(e) => e.preventDefault()}
+        type="submit"
+      >
+        Create
+      </Button>
+
+      <Button
+        className={`${appStyles.button} mx-3`}
+        onMouseDown={(e) => e.preventDefault()}
+        onClick={() => history.goBack()}
+      >
+        Cancel
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col md={7} lg={8} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+        <Col className="pt-2 pb-4 p-0 p-md-2" md={5} lg={4}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} alt="your uploaded image" rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${appStyles.button} ${styles.ButtonChangeImage} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className={`${styles.UploadPicture} d-flex justify-content-center`}
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload a picture"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                className="d-none"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+
+            {errors.image?.map((message, idx) => (
+              <Alert variant="warning" className={appStyles.Alert} key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostCreateForm;
