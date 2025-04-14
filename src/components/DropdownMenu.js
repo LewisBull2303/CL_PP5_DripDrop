bbb73fc62fdaf867ab75d4ebda677ac57253c999
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 10:45:40 +0100

    Added the dropdown menu

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -13,3 +13,40 @@ const DropdownDots = React.forwardRef(({ onClick }, ref) => (
       }}
     />
   ));
+
+/*
+  Dropdown menu for post owners to choose to edit or delete a post
+  Calls the handleEdit & handleDelete functions based on destructured props
+*/
+export const DropdownMenu = ({ handleEdit, handleDelete }) => {
+    return (
+      <Dropdown className="ml-auto" drop="left">
+        <Dropdown.Toggle as={DropdownDots} />
+  
+        <Dropdown.Menu
+          className="text-center"
+          popperConfig={{ strategy: "fixed" }} // ensures the position of the dropdown menu is consistent across all browsers
+        >
+          <OverlayTrigger placement="top" overlay={<Tooltip>Edit</Tooltip>}>
+            <Dropdown.Item
+              className={styles.DropdownItem}
+              onClick={handleEdit}
+              aria-label="edit"
+            >
+              <i className="fa-solid fa-pen"></i>
+            </Dropdown.Item>
+          </OverlayTrigger>
+  
+          <OverlayTrigger placement="top" overlay={<Tooltip>Delete</Tooltip>}>
+            <Dropdown.Item
+              className={styles.DropdownItem}
+              onClick={handleDelete}
+              aria-label="delete"
+            >
+              <i className="fas fa-trash" />
+            </Dropdown.Item>
+          </OverlayTrigger>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  };
