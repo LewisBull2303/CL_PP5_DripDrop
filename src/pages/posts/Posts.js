934cd12a7ed11d30f86f252860156995f73bb7b4
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 14:49:14 +0100

    Finalised the posts structure

--- src/pages/posts/Posts.js
+++ src/pages/posts/Posts.js
@@ -12,36 +12,36 @@ import { DropdownMenu } from "../../components/DropdownMenu";
 import FeedbackMsg from "../../components/FeedbackMsg";
 
 const Post = (props) => {
-    const {
-      id,
-      owner,
-      profile_id,
-      profile_image,
-      title,
-      description,
-      category,
-      comments_number,
-      likes_number,
-      like_id,
-      image,
-      updated_on,
-      postPage,
-      setPosts,
-    } = props;
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    title,
+    description,
+    category,
+    comments_number,
+    likes_number,
+    like_id,
+    image,
+    updated_on,
+    postPage,
+    setPosts,
+  } = props;
 
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
-    const history = useHistory();
-    const [showAlert, setShowAlert] = useState(false);
-  
-    /*
-      Handles editing of the post
-    */
-    const handleEdit = () => {
-      history.push(`/posts/${id}/edit`);
-    };
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+  const [showAlert, setShowAlert] = useState(false);
 
-    /*
+  /*
+    Handles editing of the post
+  */
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
+
+  /*
     Handles deleting of the post
     Shows the confirmation message to the user
     Redirects the user to the main page after a short delay
@@ -58,28 +58,28 @@ const Post = (props) => {
     }
   };
 
-    /*
+  /*
     Handles liking of the post by the user
     Sends a request to the API for a post with a specific id
     Increments the likes number by 1
   */
-    const handleLike = async () => {
-        try {
-          const { data } = await axiosRes.post("/likes/", { post: id });
-          setPosts((prevPosts) => ({
-            ...prevPosts,
-            results: prevPosts.results.map((post) => {
-              return post.id === id
-                ? { ...post, likes_number: post.likes_number + 1, like_id: data.id }
-                : post;
-            }),
-          }));
-        } catch (err) {
-          // console.log(err);
-        }
-      };
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_number: post.likes_number + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      // console.log(err);
+    }
+  };
 
-      /*
+  /*
     Handles unliking of the post already liked by the user
     Sends a request to the API for a post with a specific id
     Decrements the likes number by 1
@@ -98,7 +98,6 @@ const Post = (props) => {
     } catch (err) {
       // console.log(err);
     }
-    
   };
 
   return (
@@ -120,12 +119,81 @@ const Post = (props) => {
         <div className={styles.UpdatedOn}>{updated_on}</div>
 
         {/* Display the edit dropdown menu for owner of the post and if the
-        postPage prop exists */}
+        postPage item exists */}
         <div className={styles.EditIcon}>
           {is_owner && postPage && (
             <DropdownMenu handleEdit={handleEdit} handleDelete={handleDelete} />
           )}
         </div>
       </Card.Body>
-    
-  
+
+      <Link to={`/posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+
+      <Card.Body>
+        <Card.Title className="text-center">{title}</Card.Title>
+        {description && <Card.Text>{description}</Card.Text>}
+        <Card.Text>
+          Type:
+          <Badge variant="secondary" className={styles.BadgePost}>
+            {category}
+          </Badge>
+        </Card.Text>
+        <hr className={appStyles.Line} />
+
+        <div>
+          {
+            is_owner ? (
+              // users cannot like their own posts
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip>This is your post, you cannot like it </Tooltip>
+                }
+              >
+                <img
+                  src={Unlike}
+                  className={appStyles.LikeIcon}
+                  alt="Like hand"
+                  height="35"
+                  width="35"
+                />
+              </OverlayTrigger>
+            ) : like_id ? (
+              // check if the user has already liked the post
+              <span onClick={handleUnlike}>
+                <img
+                  src={Like}
+                  className={appStyles.LikeIcon}
+                  alt="Like hand"
+                  height="35"
+                  width="35"
+                />
+              </span>
+            ) : currentUser ? (
+              // ability to like the post
+              <span onClick={handleLike}>
+                <img
+                  src={Unlike}
+                  className={appStyles.LikeIcon}
+                  alt="Unlike hand"
+                  height="35"
+                  width="35"
+                />
+              </span>
+            ) : null // user must be logged in to the app to view the content
+          }
+          <span className="ml-1">{likes_number}</span>
+
+          <Link to={`/posts/${id}`} aria-label="view comments">
+            <i className="fa-regular fa-comment ml-2" />
+          </Link>
+          <span className="ml-1">{comments_number}</span>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Post;
