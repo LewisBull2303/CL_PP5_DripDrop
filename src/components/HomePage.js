0c0f8fc06abed8693afb3f140579a7bf48bcd014
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 10:59:41 +0100

    Added my home page.js and added the imports, also imported my home page image

---
+++ src/components/HomePage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Card, Button, Row, Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import homeImage from "../assets/home-page-image.png";
+import styles from "../styles/Landing.module.css";
+import appStyles from "../App.module.css";
