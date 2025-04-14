dfa57ed325dd4c9d7746e1d63663052ed13721f5
Lewis Bull <lewisbull2303@gmail.com>
2025-04-14 16:35:04 +0100

    Added the main post page and added the imports

---
+++ src/pages/posts/MainPostsPage.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { Badge, Col, Container, Form, Row } from "react-bootstrap";
+import styles from "../../styles/MainPostsPage.module.css";
+import columnStyles from "../../styles/SmallMenuContainer.module.css";
+import appStyles from "../../App.module.css";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import NoResultsImage from "../../assets/no-results-found.png";
+import LikeFeedAddPost from "../../components/LikeFeedAddPost";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
