42b4334b41c6ffdd76ef5a75a2f95f682754ee9a
Lewis Bull <lewisbull2303@gmail.com>
2025-04-15 09:54:11 +0100

    Made the comment create form function and added the handle change function

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -4,3 +4,15 @@ import { Button, Form, InputGroup } from "react-bootstrap";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+
+function CommentCreateForm(props) {
+    const { post, setPost, setComments, profileImage, profile_id } = props;
+    const [content, setContent] = useState("");
+  
+    /* 
+      Handles changes to the create comment input field
+    */
+    const handleChange = (e) => {
+      setContent(e.target.value);
+    };
+}  
