ef09798e3f9ec0ced37bb56878e3e7decfd32233
Lewis Bull <lewisbull2303@gmail.com>
2025-04-15 14:52:42 +0100

    Added the correct api link for the axis default

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "https://"
+axios.defaults.baseURL = "https://dripdrop-drf-api-8425aae53a44.herokuapp.com/"
 // Set the expected data format by the api
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // Set to avoid any CORS errors when sending cookies
