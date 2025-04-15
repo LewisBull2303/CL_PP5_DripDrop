91b6802e3a5cb84a344331959583db7a94f442a6
Lewis Bull <lewisbull2303@gmail.com>
2025-04-15 10:24:14 +0100

    Added the comment edit form function and the handle change function

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -2,3 +2,15 @@ import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { axiosRes } from "../../api/axiosDefaults";
 import styles from "../../styles/CommentCreateEditForm.module.css";
+
+function CommentEditForm(props) {
+    const { id, content, setShowEditForm, setComments, setShowAlert } = props;
+    const [formContent, setFormContent] = useState(content);
+  
+    /* 
+      Handles changes to form input
+    */
+    const handleChange = (e) => {
+      setFormContent(e.target.value);
+    };
+}
