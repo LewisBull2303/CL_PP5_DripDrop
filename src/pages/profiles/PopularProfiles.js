13c1dfee47480625be28cff0106875ef4cffbbe9
Lewis Bull <lewisbull2303@gmail.com>
2025-04-15 16:42:37 +0100

    Imported profile correctly

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/SmallMenuContainer.module.css";
 import Asset from "../../components/Asset";
-import Profile from "./Profile";
+import Profile from ".Profile";
 import { useProfileData } from "../../contexts/ProfileDataContext";
 
 /*
