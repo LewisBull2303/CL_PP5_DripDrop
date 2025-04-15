eb7712f2db49df531e356a0ec0e0a6fa4346d442
Lewis Bull <lewisbull2303@gmail.com>
2025-04-15 16:16:53 +0100

    chanegd the import for login form

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import styles from "../../styles/LoginSignupForm.module.css";
+import styles from "../../styles/LogInSignupForm.module.css";
 import appStyles from "../..App.module.css";
 import { Alert, Form, Button, Col, Row, Container } from "react-bootstrap";
 import axios from "axios";
