a6855494c19fbce6732b7c0eb033a31ef2a8b5b0
Lewis Bull <lewisbull2303@gmail.com>
2025-04-15 16:12:07 +0100

    changed the improt for the main styles in loginform

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/LoginSignupForm.module.css";
-import appStyles from "../../main.styles.css";
+import appStyles from "../..App.module.css";
 import { Alert, Form, Button, Col, Row, Container } from "react-bootstrap";
 import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
