d090a4490706890da953368121684bddba83752b
Lewis Bull <lewisbull2303@gmail.com>
2025-04-16 13:13:09 +0100

    Updated my use redirect to fix an error

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,28 +1,33 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
+import { shouldRefreshToken } from "../utils/utils";
 
 export const useRedirect = (userAuthStatus) => {
-    const history = useNavigate();
-    /*
-    Checks if the user is logged in
-    if yes, refreshes the access token
-    Redirects the user to the home page
-  */
-    useEffect(() => {
-        const handleMount = async () => {
-          try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-            if (userAuthStatus === "loggedIn") {
-              history.push("/");
-            }
-          } catch (err) {
-            if (userAuthStatus === "loggedOut") {
-              history.push("/");
-            }
+  const history = useNavigate();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      const hasRefreshToken = shouldRefreshToken();
+
+      if (hasRefreshToken) {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+          if (userAuthStatus === "loggedIn") {
+            history("/");
           }
-        };
-    
-        handleMount();
-      }, [history, userAuthStatus]);
+        } catch (err) {
+          if (userAuthStatus === "loggedOut") {
+            history("/");
+          }
+        }
+      } else {
+        if (userAuthStatus === "loggedOut") {
+          history("/");
+        }
+      }
     };
+
+    handleMount();
+  }, [history, userAuthStatus]);
+};
