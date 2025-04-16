ab58bb1fcc68f1dbe7cbf73d4fe402e610501fcd
Lewis Bull <lewisbull2303@gmail.com>
2025-04-16 15:38:07 +0100

    Reverted hte useRedirect to try again

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,30 +1,25 @@
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { useEffect } from "react";
-import { shouldRefreshToken } from "../utils/utils";
+import { useNavigate } from "react-router";
 
 export const useRedirect = (userAuthStatus) => {
   const history = useNavigate();
 
+  /*
+    Checks if the user is currently logged in or not
+    Refreshes the access token if a user is logged in
+    Redirects the user to the home page
+  */
   useEffect(() => {
     const handleMount = async () => {
-      const hasRefreshToken = shouldRefreshToken();
-
-      // If the user is not logged in at all, don't try to refresh
-      if (!hasRefreshToken) {
-        if (userAuthStatus === "loggedOut") {
-          history("/");
-        }
-        return;
-      }
-
       try {
         await axios.post("/dj-rest-auth/token/refresh/");
         if (userAuthStatus === "loggedIn") {
-          history("/");
+          history.push("/");
         }
       } catch (err) {
         if (userAuthStatus === "loggedOut") {
-          history("/");
+          history.push("/");
         }
       }
     };
