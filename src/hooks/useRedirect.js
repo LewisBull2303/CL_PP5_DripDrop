ff9bde38c1fd039894b810c8b062e10c54a07844
Lewis Bull <lewisbull2303@gmail.com>
2025-04-17 10:50:02 +0100

    Updated the useredirect

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,24 +1,24 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
+/**
+ * Redirects the user based on authentication status.
+ * 
+ * @param {"loggedIn" | "loggedOut"} userAuthStatus
+ * If "loggedIn", redirects to "/" if the user IS logged in.
+ * If "loggedOut", redirects to "/" if the user is NOT logged in.
+ */
 export const useRedirect = (userAuthStatus) => {
-  const history = useNavigate();
+  const currentUser = useCurrentUser();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const handleMount = async () => {
-      try {
-        await axios.post("/dj-rest-auth/token/refresh/");
-        if (userAuthStatus === "loggedIn") {
-          history("/");
-        }
-      } catch (err) {
-        if (userAuthStatus === "loggedOut") {
-          history("/");
-        }
-      }
-    };
-
-    handleMount();
-  }, [history, userAuthStatus]);
+    if (
+      (userAuthStatus === "loggedIn" && currentUser) ||
+      (userAuthStatus === "loggedOut" && !currentUser)
+    ) {
+      navigate("/");
+    }
+  }, [navigate, userAuthStatus, currentUser]);
 };
