503cf463d4b7d8636cc70e287e95e979482ba2c6
Lewis Bull <lewisbull2303@gmail.com>
2025-04-17 16:37:13 +0100

    uncommented out the error catch

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -24,7 +24,7 @@ export const CurrentUserProvider = ({ children }) => {
       console.log("WORKING CORRECTLY 1")
       setCurrentUser(data);
     } catch (err) {
-      // console.log(err);
+        console.log(err);
     }
   };
 
