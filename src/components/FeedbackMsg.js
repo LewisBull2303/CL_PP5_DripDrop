acf09e0ac141a94bafaa28cf714792479c40673c
Lewis Bull <lewisbull2303@gmail.com>
2025-04-17 09:54:44 +0100

    Fixed indentaion for the feedbackmsg

--- src/components/FeedbackMsg.js
+++ src/components/FeedbackMsg.js
@@ -5,24 +5,24 @@ import React from "react";
 import { useState } from "react";
 
 export default function FeedbackMsg({ variant, message }) {
-    const [isShown, setIsShown] = useState(true);
-  
-    /*
-      Handles closing of the alert message
-    */
-    const handleClose = (e) => {
-      e.preventDefault();
-      setIsShown(false);
-    };
-  
-    return (
-      <div
-        className={css(styles.alert, styles[variant], !isShown && styles.hide)}
-      >
-        <span className={styles.closebtn} onClick={handleClose}>
-          &times;
-        </span>
-        {message}
-      </div>
-    );
-  }
+  const [isShown, setIsShown] = useState(true);
+
+  /*
+    Handles closing of the alert message
+  */
+  const handleClose = (e) => {
+    e.preventDefault();
+    setIsShown(false);
+  };
+
+  return (
+    <div
+      className={css(styles.alert, styles[variant], !isShown && styles.hide)}
+    >
+      <span className={styles.closebtn} onClick={handleClose}>
+        &times;
+      </span>
+      {message}
+    </div>
+  );
+}
